c8bbefef3a75da0f3c23a462ec5ca2d2e22daf15
Jaka Hudoklin <jakahudoklin@gmail.com>
2016-03-11 15:45:27 +0100

    redis: fix indents

--- redis.nix
+++ redis.nix
@@ -1,54 +1,53 @@
 { config, lib, ... }:
 
- with lib;
+with lib;
 
- let
-   cfg = config.services.redis;
- in {
-   options.services.redis = {
-     enable = mkEnableOption "redis service";
+let
+  cfg = config.services.redis;
+in {
+  options.services.redis = {
+    enable = mkEnableOption "redis service";
 
-     password = mkOption {
-       description = "Auth password";
-       type = types.nullOr types.str;
-       default = null;
-     };
+    password = mkOption {
+      description = "Auth password";
+      type = types.nullOr types.str;
+      default = null;
+    };
 
-     persistent = mkOption {
-       description = "Whether to persist data";
-       type = types.bool;
-       default = true;
-     };
-   };
+    persistent = mkOption {
+      description = "Whether to persist data";
+      type = types.bool;
+      default = true;
+    };
+  };
 
-   config = mkIf cfg.enable (mkMerge [
-   {
-     kubernetes.controllers.redis = {
-       dependencies = ["services/redis"];
+  config = mkIf cfg.enable (mkMerge [
+  {
+    kubernetes.controllers.redis = {
+      dependencies = ["services/redis"];
 
-       pod.containers.redis = {
-         image = "redis";
-         args = "redis-server --appendonly yes ${optionalString (cfg.password != null) "--requirepass ${cfg.password}"}";
-         mounts = mkIf cfg.persistent [{
-           name = "storage";
-           mountPath = "/data";
-         }];
-         ports = [{ port = 6379; }];
-       };
-     };
+      pod.containers.redis = {
+        image = "redis";
+        args = "redis-server --appendonly yes ${optionalString (cfg.password != null) "--requirepass ${cfg.password}"}";
+        mounts = mkIf cfg.persistent [{
+          name = "storage";
+          mountPath = "/data";
+        }];
+        ports = [{ port = 6379; }];
+      };
+    };
 
-     kubernetes.services.redis.ports = [{ port = 6379; }];
-   } (mkIf cfg.persistent {
-     kubernetes.controllers.redis = {
-       dependencies = ["pvc/redis"];
+    kubernetes.services.redis.ports = [{ port = 6379; }];
+  } (mkIf cfg.persistent {
+    kubernetes.controllers.redis = {
+      dependencies = ["pvc/redis"];
 
-       pod.volumes.storage = {
-         type = "persistentVolumeClaim";
-         options.claimName = "redis";
-       };
-     };
+      pod.volumes.storage = {
+        type = "persistentVolumeClaim";
+        options.claimName = "redis";
+      };
+    };
 
-     kubernetes.pvc.redis.size = "1G";
-   })]
-  );
- }
+    kubernetes.pvc.redis.size = "1G";
+  })]);
+}

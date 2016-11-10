1507456ce2e7a8ab69e0c6d6b0aa69b9d7941fd1
Jaka Hudoklin <jakahudoklin@gmail.com>
2016-11-10 15:14:28 +0100

    elasticsearch: incrise resource requirements

--- elasticsearch.nix
+++ elasticsearch.nix
@@ -17,17 +17,21 @@
    config = mkIf cfg.enable {
      kubernetes.deployments.elasticsearch = {
        dependencies = ["services/elasticsearch" "pvc/elasticsearch"];
+
        pod.containers.elasticsearch = {
-         image = "elasticsearch";
+         image = "gatehub/elasticsearch";
          ports = [{ port = 9200; }];
          mounts = [{
            name = "storage";
            mountPath = "/usr/share/elasticsearch/data";
          }];
 
-         requests.memory = "512Mi";
-         requests.cpu = "250m";
-         limits.memory = "1024Mi";
+         env = {
+            ES_HEAP_SIZE = "2g";
+         };
+
+         requests.memory = "4096Mi";
+         requests.cpu = "1000m";
 
          security.capabilities.add = ["IPC_LOCK"];
        };
